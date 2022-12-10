ff8aab885dbb01f8ff3ca97729a1bca9ad6c3928
Your Name <you@example.com>
2022-12-10 05:11:44 +0000

    devise dev ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_005447) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_025014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_005447) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "occupations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "day"
+    t.datetime "time_s"
+    t.datetime "time_e"
+    t.boolean "reservation"
+    t.boolean "confirmed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_occupations_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.integer "number"
@@ -60,4 +72,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_005447) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "occupations", "users"
 end
